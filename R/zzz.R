1571890d5f4434686f92cf8527afbfe9c8db7b3b
przemol <pstempor@gmail.com>
2014-10-28 19:47:48 +0000

    test for db connection on startup

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,16 @@
 .onAttach <- function(libname, pkgname) {
     #Fix for annoying BiocGenerics namespace problems
     if(!"package:BiocGenerics" %in% search()) attachNamespace('BiocGenerics')
-    packageStartupMessage('Loaded')
+    
+    con <- dbConnect(dbDriver("MySQL"), group = "jadb")
+    ok <- dbExistsTable(con, 'labfiles')
+    dbDisconnect(con)
+    if( ok ) {
+        packageStartupMessage('Package loaded, DB connection OK.')
+    } else {
+        stop('DB connection problem. Configure jadb group in $HOME/.my.cnf.')
+    }
+    
 }
 
 
