d8551947830c2ab8b0ccfeec7f3ca93d3acc52f4
przemol <pstempor@gmail.com>
2014-10-28 17:38:32 +0000

    JADBtools package created

---
+++ R/JADBtools-package.R
@@ -0,0 +1,13 @@
+#' @docType JADBtools
+#' @name seqplots
+#' 
+#' @title 
+#' JADB I/O tools
+#' 
+#' @description
+#' Tools for retrive and manipulate data in JADB.
+#' 
+#' @author Przemyslaw Stempor
+#' 
+#' @import XML DBI RMySQL digest rtracklayer
+NULL
