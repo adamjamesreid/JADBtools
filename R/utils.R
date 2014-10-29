04e477cd6a628f65b5f1a42b294d2df13ff85c04
przemol <pstempor@gmail.com>
2014-10-29 16:47:50 +0000

    bug fix fo pb function

--- R/utils.R
+++ R/utils.R
@@ -31,6 +31,6 @@ pb <- function(
     mnt, uid, s=TRUE, p=0.01, t='Loading libraries...', tt=''
 ) {
     cat( toJSON(
-        list(success=s, progress=p, t=text, tt=toptext)
+        list(success=s, progress=p, text=t, toptext=tt)
     ), file=paste(mnt, '/temp/', uid, '.json', sep='') )
 }
