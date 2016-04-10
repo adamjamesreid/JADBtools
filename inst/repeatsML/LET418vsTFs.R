0d3a749e59a074eef4b22d8430e47b16b9681174
przemol <pstempor@gmail.com>
2016-04-10 03:01:13 +0100

    script to find TFs enriched on let418 missregulated genes

--- inst/repeatsML/LET418vsTFs.R
+++ inst/repeatsML/LET418vsTFs.R
@@ -1,5 +1,41 @@
-files <- dir('/Volumes/data/_HOT_jointAWG_worm_fly_human/worm_Ce10_noDoubleCounting/source_data')
+require(magrittr)
+
+files <- dir('/Volumes/data/_HOT_jointAWG_worm_fly_human/worm_Ce10_noDoubleCounting/source_data', full.names = TRUE)
 stage <- strsplit(dir('/Volumes/data/_HOT_jointAWG_worm_fly_human/worm_Ce10_noDoubleCounting/source_data'),'_')  %>% sapply('[[', 3)
 factor <- strsplit(dir('/Volumes/data/_HOT_jointAWG_worm_fly_human/worm_Ce10_noDoubleCounting/source_data'),'_')  %>% sapply('[[', 2)
 
-reg <- ChIPseeker::readPeakFile(files[1], header=FALSE)
+lst <- lapply(files, ChIPseeker::readPeakFile, header=FALSE)
+names(lst) <- basename(files)
+modencodetfbs <- GRangesList(lst)
+
+let <- lapply(dir('inst/let418/', pattern = 'bed', full.names = TRUE)[-1], import.bed)
+let <- GRangesList(let)
+names(let) <- dir('inst/let418/', pattern = 'bed')
+int <- Reduce(intersect, let)
+uni <- unique(unlist(let))
+
+ovr <- findOverlaps(promoters(uni, upstream = 500, downstream = 100), modencodetfbs)
+
+#% of genes having annotated TF
+length(unique(queryHits(ovr))) / length(uni)
+
+#% of genes having annotated TF
+length(unique(queryHits(ovr))) / length(uni)
+
+sort(factor[subjectHits(ovr)]  %>% table, decreasing = TRUE)
+
+sort(factor[subjectHits(ovr)]  %>% table, decreasing = TRUE) / length(uni)
+
+
+
+o2 <- findOverlaps(promoters(int, upstream = 500, downstream = 100), modencodetfbs)
+sort(factor[subjectHits(o2)]  %>% table, decreasing = TRUE) / length(int)
+
+
+hi <- import.bed('http://ws190.gurdon.private.cam.ac.uk:3838/seqplots/files/TSS_top_20pct.bed')
+ovrR <- findOverlaps(promoters(sample(hi, 10), upstream = 500, downstream = 100), modencodetfbs)
+sort(factor[subjectHits(ovrR)]  %>% table, decreasing = TRUE) / length(uni)
+
+
+unique(factor)  %>%  sapply(function(x) length(unlist(modencodetfbs[factor==x])) ) -> n
+
--- inst/repeatsML/RepeatStat.R
+++ inst/repeatsML/RepeatStat.R
@@ -12,3 +12,13 @@ p + geom_violin(aes(fill = factor(nam)), scale = "width") + scale_y_log10()
 rep <- unlist(repeatModel)
 peak <- unlist(reduce(liftOver(rep, chain), min.gapwidth=50))
 peakAnno <- annotatePeak(peak, tssRegion=c(-500, 500), TxDb=txdb)
+
+#families
+unique(unlist(repeatModel)$classification_tags)  %>%  length()
+
+#number
+length(unlist(repeatModel))
+
+
+#number
+length(unlist(repeatModel))
