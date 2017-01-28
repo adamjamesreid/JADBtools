e738f95850c3bc9ad9359001ed06052eb6698479
Przemol <pstempor@gmail.com>
2017-01-28 16:19:36 +0000

    beads mapq0 tracks

--- R/AddTracksTools.R
+++ R/AddTracksTools.R
@@ -1,3 +1,65 @@
+#' addMapq0Beads
+#' 
+#' @param IDs Vector of JADB ContactExpIDs
+#'   
+#' @return List 
+#' 
+#' @author Przemyslaw Stempor
+#' 
+#' @family tracks
+#' @export
+#' 
+#' @examples
+#' #addMapq0Beads('AA001')
+addMapq0Beads <- function(ids) {
+    require(magrittr)
+    require(Rsamtools)
+    require(BSgenome)
+    
+    base_dir  <- getwd()
+    on.exit(setwd(base_dir))
+    
+    ids  %>% sapply(getFilePath, format = "bam", eq=TRUE, processing = "aligned", scale = "NA", url = FALSE)  -> fls
+    if (length(ids) != 1) stop('No or more than 1 BAM files.')
+    
+    outnames <- sapply(basename(fls), rbeads:::reName, proccesing = 'mapq0', scale = 'linear', resolution = '1bp', ext='.bw')
+    prefix <- basename(fls) %>% substr(start=0, stop=nchar(.)-13)
+    
+    exp_dir <- gsub('files/', '', dirname(fls))
+    message(exp_dir)
+    setwd(exp_dir)
+
+    crosslink <- JADBtools::getAnno(ids, anno = 'Crosslinker', EXTABLE = 'labexperimentview')
+    if(grepl('^e', crosslink, ignore.case = TRUE)) {
+        input <- 'Input/SummedInputs/HISeq_EGS_map0_NOTuniq_SummedInput_linear_25bp_SummedInput_linear_25bp.bw'
+    }  else {
+        input <- 'Input/SummedInputs/HISeq_FRM_map0_NOTuniq_SummedInput_linear_25bp_SummedInput_linear_25bp.bw'
+    }
+
+    message('File: ', basename(fls), '\n vs. ', basename(input))
+    
+    beads(
+        basename(fls), 
+        input, 
+        '../../BEADS/map1.bw', 'ce10', 
+        uniq = FALSE, insert = 200L, mapq_cutoff = 0, export = "BEADS", 
+        rdata = FALSE, export_er = TRUE, quickMap = TRUE
+    )
+    
+    Entry <- addGenericFile(
+        ids,
+        path = file.path('files', exp_dir, gsub('aligned\\^NA\\^NA', 'mapq0^linear^1bp', prefix)), 
+        Processing = 'mapq0', 
+        Scale = 'linear', 
+        Resolution = '1bp',
+        filetype_format = 'bw', 
+        prefix = 'P',
+        comments = JADBtools::bamStats(basename(fls))
+    )
+    
+}
+
+
 #' addMapq0Track
 #' 
 #' @param IDs Vector of JADB ContactExpIDs
