0c06481617e5041b2394fc67f72f59c15f4a808c
przemol <pstempor@gmail.com>
2017-07-20 16:38:57 +0100

    fs accesss functions

---
+++ R/fs_access_functions.R
@@ -0,0 +1,50 @@
+#' jadb_get_file_paths
+#'
+#' @param ids ContactExpIDs
+#' @param processing raw
+#' @param root_prefix /mnt/jadb/DBfile/DBfiles
+#'
+#' @return paths
+#' @export
+#'
+jadb_get_file_paths <- function(
+    ids, processing='raw', root_prefix='/mnt/jadb/DBfile/DBfiles'
+) {
+    
+    if(nchar(root_prefix)) {
+        pth <- sapply(ids, getFilePath, processing=processing, url=FALSE)
+        pth <- gsub('^files', root_prefix, pth)
+    } else {
+        pth <- sapply(ids, getFilePath, processing=processing)
+    }
+    return(pth)
+}
+
+
+#' jadb_download_files
+#'
+#' @param ids ContactExpIDs
+#' @param processing raw
+#'
+#' @return TRUE if downloaded
+#' @export
+#'
+jadb_download_files <- function(ids, processing='raw') {
+    
+    pth <- jadb_get_file_paths(ids, root_prefix = '', processing=processing)
+    sapply(pth, function(x) download.file(x, basename(x)))
+}
+
+
+#' make_jadb_ids
+#'
+#' @param ran range
+#' @param prefix prefix
+#'
+#' @return ids
+#' @export
+#'
+make_jadb_ids <- function(ran, prefix='rYD') {
+    sprintf('%s%0.3i', prefix, ran)
+}
+    
