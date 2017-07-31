23be4e67fa06c232ff3148680932a5ec05c9bf49
przemol <pstempor@gmail.com>
2017-07-31 16:27:30 +0100

    summed inputs for ce10

--- inst/beads_support/SummedInputs.R
+++ inst/beads_support/SummedInputs.R
@@ -47,3 +47,64 @@ sumBAMinputs(
     quickMap = TRUE,
     bin = 25L
 )
+
+
+
+## ce10 
+
+
+#ce10_FRM_HiSeq_nonUNIQ_MAPQ10_200bp_SummedInput_bin25bp.bw
+sumBAMinputs(
+    bam.controls = "ce10_FRM_HiSeq_input.bam", 
+    bw.mappability = '../../../_mappability_files_/ce10_gem-mappability_36bp.bw', 
+    genome = 'ce10',
+    out_name = "ce10_FRM_HiSeq_nonUNIQ_MAPQ10_200bp",
+    uniq = FALSE, 
+    insert = 200L, 
+    mapq_cutoff = 10L, 
+    quickMap = TRUE,
+    bin = 25L
+)
+file.rename('ce10_FRM_HiSeq_nonUNIQ_MAPQ10_200bp_SummedInput_linear_25bp.bw', 'ce10_FRM_HiSeq_nonUNIQ_MAPQ10_200bp_SummedInput_bin25bp.bw')
+
+
+sumBAMinputs(
+    bam.controls = "ce10_EGS_HiSeq_input.bam", 
+    bw.mappability = '../../../_mappability_files_/ce10_gem-mappability_36bp.bw', 
+    genome = 'ce10',
+    out_name = "ce10_EGS_HiSeq_nonUNIQ_MAPQ10_200bp",
+    uniq = FALSE, 
+    insert = 200L, 
+    mapq_cutoff = 10L, 
+    quickMap = TRUE,
+    bin = 25L
+)
+file.rename('ce10_EGS_HiSeq_nonUNIQ_MAPQ10_200bp_SummedInput_linear_25bp.bw', 'ce10_EGS_HiSeq_nonUNIQ_MAPQ10_200bp_SummedInput_bin25bp.bw')
+
+
+sumBAMinputs(
+    bam.controls = "ce10_FRM_HiSeq_input.bam", 
+    bw.mappability = '../../../_mappability_files_/ce10_gem-mappability_36bp.bw', 
+    genome = 'ce10',
+    out_name = "ce10_FRM_HiSeq_nonUNIQ_nonMAPQ_200bp",
+    uniq = FALSE, 
+    insert = 200L, 
+    mapq_cutoff = 0L, 
+    quickMap = TRUE,
+    bin = 25L
+)
+file.rename('ce10_FRM_HiSeq_nonUNIQ_nonMAPQ_200bp_SummedInput_linear_25bp.bw', 'ce10_FRM_HiSeq_nonUNIQ_nonMAPQ_200bp_SummedInput_bin25bp.bw')
+
+
+sumBAMinputs(
+    bam.controls = "ce10_EGS_HiSeq_input.bam", 
+    bw.mappability = '../../../_mappability_files_/ce10_gem-mappability_36bp.bw', 
+    genome = 'ce10',
+    out_name = "ce10_EGS_HiSeq_nonUNIQ_nonMAPQ_200bp",
+    uniq = FALSE, 
+    insert = 200L, 
+    mapq_cutoff = 0L, 
+    quickMap = TRUE,
+    bin = 25L
+)
+file.rename('ce10_EGS_HiSeq_nonUNIQ_nonMAPQ_200bp_SummedInput_linear_25bp.bw', 'ce10_EGS_HiSeq_nonUNIQ_nonMAPQ_200bp_SummedInput_bin25bp.bw')
