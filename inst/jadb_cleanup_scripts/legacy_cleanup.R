c2e477ef8850701c8ec0b3ea27a01b993acc1b99
JA_Lab Pipeline <jarun@cb-head2.gurdon.private.cam.ac.uk>
2017-10-25 18:28:33 +0100

    update scripts

--- inst/jadb_cleanup_scripts/legacy_cleanup.R
+++ inst/jadb_cleanup_scripts/legacy_cleanup.R
@@ -19,15 +19,123 @@ jar_log()
 
 
 
+#' # jacl_send_to_cluster("AA691", basespace_addr="https://docs.google.com/spreadsheets/d/1QpWQxl3WDL1hRHfJLOlql5qsGWPyFTosCBmJQWTQiQU/edit?usp=sharing")
+scl <- function(ID, genome='ce10', ops=', purge=TRUE, backup=TRUE', out_sufix=NULL, pipeline='jadb_ChIPseq') {
+    message(ID)
+    
+    if(is.null(out_sufix)) {
+        out_sufix <- pipeline
+    }
+    
+    cmd_lst <- c(
+        "echo '#!/usr/bin/Rscript",
+        'Sys.info();',
+        if(genome=='ce10') 'Sys.setenv(JADB_GROUP=\"ja-db\")' else '',
+        if(genome=='ce10') 'Sys.setenv(JADB_MOUNT=\"/mnt/jadb2/DBfile/DBfiles\")' else '',
+        'library(JADBtools);',
+        'logdir <- file.path(MOUNT, \"_log\");',
+        
+        sprintf('%s(\"%s\", genome=\"%s\"%s);', pipeline, ID, genome, ops),
+        
+        'setwd(logdir);',
+        sprintf('file.rename(\"%s.%s\", \"done/%s.%s\");', ID, out_sufix, ID, out_sufix),
+        "'"
+    )
+    
+    cmd <- sprintf(
+        "%s | sbatch --job-name=%s --output=%s/%s.%s --ntasks-per-node=8 --nice=1000", #--exclude=node9
+        paste0(cmd_lst, collapse = '\n'), ID, 
+        file.path(MOUNT, '_log'), 
+        ID, out_sufix
+    )
+    message(cmd)
+    system(cmd)
+    #z <- ssh.utils::run.remote(cmd, remote='localhost', verbose = TRUE)
+    #message(z$cmd.out)
+    
+    
+    
+    
+}
+
+sc2l <- function(ID, genome='ce11', ops=', steps = \'map10_uniq\', purge = FALSE, backup = FALSE', out_sufix='uniq_beads', pipeline='jadb_ChIPseq') {
+    message(ID)
+    
+    if(is.null(out_sufix)) {
+        out_sufix <- pipeline
+    }
+    
+    cmd_lst <- c(
+        "echo '#!/usr/bin/Rscript",
+        'Sys.info();',
+        'library(JADBtools);',
+        'logdir <- file.path(MOUNT, \"_log\");',
+        
+        sprintf('%s(\"%s\", genome=\"%s\"%s);', pipeline, ID, genome, ops),
+        
+        'setwd(logdir);',
+        sprintf('file.rename(\"%s/%s.%s\", \"%s/done/%s.%s\");', file.path(MOUNT, '_log'), ID, out_sufix, file.path(MOUNT, '_log'), ID, out_sufix),
+        "'"
+    )
+    
+    cmd <- sprintf(
+        "%s | sbatch --job-name=%s --output=%s/%s.%s --ntasks-per-node=8 --nice=10", #--exclude=node9
+        paste0(cmd_lst, collapse = '\n'), ID, 
+        file.path(MOUNT, '_log'), 
+        ID, out_sufix
+    )
+    message(cmd)
+    system(cmd)
+
+    
+    
+    
+    
+}
+
+
+
 
 log <- jar_nlog()
 log[!log %in% jar_squeue()$NAME]
 
 
-detach("package:JADBtools", unload=TRUE)
-Sys.setenv(JADB_GROUP="ja-db")
-Sys.setenv(JADB_MOUNT="/mnt/jadb2/DBfile/DBfiles")
-require(JADBtools)
+set_ce10 <- function(x) {
+    detach("package:JADBtools", unload=TRUE)
+    Sys.setenv(JADB_GROUP="ja-db")
+    Sys.setenv(JADB_MOUNT="/mnt/jadb2/DBfile/DBfiles")
+    require(JADBtools)
+}
+
+set_ce11 <- function(x) {
+    detach("package:JADBtools", unload=TRUE)
+    Sys.setenv(JADB_GROUP="jadb")
+    Sys.setenv(JADB_MOUNT="/mnt/jadb/DBfile/DBfiles")
+    require(JADBtools)
+}
+
+ja_switch <- function() {
+    if(GROUP=="jadb") {
+        GROUP <<- "ja-db"
+        MOUNT <<- "/mnt/jadb2/DBfile/DBfiles"
+    } else {
+        GROUP <<- "jadb"
+        MOUNT <<- "/mnt/jadb/DBfile/DBfiles"
+    }
+    message('Switched to: [', GROUP, '] mouned on [', MOUNT, ']')
+}
+
+
+
+
+backupFiles <- function(id) {
+    pth <- getFilePath(id, url = FALSE)
+    dirname <- gsub('files', MOUNT, unique(dirname(pth)))
+    
+    bck <- gsub('DBfiles', 'DBfiles_legacy', dirname)
+    dir.create(bck, recursive = TRUE)
+    file.copy(file.path(dirname, '/'), bck, recursive = TRUE)
+}
 
 
 log <- jar_nlog(db='jadb2')
