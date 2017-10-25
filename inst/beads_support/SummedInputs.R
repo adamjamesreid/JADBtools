65b983a9431739966ffa7a44205669e741e82a7f
JA_Lab Pipeline <jarun@cb-head2.gurdon.private.cam.ac.uk>
2017-10-25 18:28:11 +0100

    summed inputs for UNIQ

--- inst/beads_support/SummedInputs.R
+++ inst/beads_support/SummedInputs.R
@@ -108,3 +108,65 @@ sumBAMinputs(
     bin = 25L
 )
 file.rename('ce10_EGS_HiSeq_nonUNIQ_nonMAPQ_200bp_SummedInput_linear_25bp.bw', 'ce10_EGS_HiSeq_nonUNIQ_nonMAPQ_200bp_SummedInput_bin25bp.bw')
+
+
+
+### uniq
+
+#ce10_FRM_HiSeq_nonUNIQ_MAPQ10_200bp_SummedInput_bin25bp.bw
+sumBAMinputs(
+    bam.controls = "ce10_FRM_HiSeq_input.bam", 
+    bw.mappability = '../../../_mappability_files_/ce10_gem-mappability_36bp.bw', 
+    genome = 'ce10',
+    out_name = "ce10_FRM_HiSeq_UNIQ_MAPQ10_200bp",
+    uniq = TRUE, 
+    insert = 200L, 
+    mapq_cutoff = 10L, 
+    quickMap = TRUE,
+    bin = 25L
+)
+file.rename('ce10_FRM_HiSeq_UNIQ_MAPQ10_200bp_SummedInput_linear_25bp.bw', 'ce10_FRM_HiSeq_UNIQ_MAPQ10_200bp_SummedInput_bin25bp.bw')
+
+
+sumBAMinputs(
+    bam.controls = "ce10_EGS_HiSeq_input.bam", 
+    bw.mappability = '../../../_mappability_files_/ce10_gem-mappability_36bp.bw', 
+    genome = 'ce10',
+    out_name = "ce10_EGS_HiSeq_UNIQ_MAPQ10_200bp",
+    uniq = TRUE, 
+    insert = 200L, 
+    mapq_cutoff = 10L, 
+    quickMap = TRUE,
+    bin = 25L
+)
+file.rename('ce10_EGS_HiSeq_UNIQ_MAPQ10_200bp_SummedInput_linear_25bp.bw', 'ce10_EGS_HiSeq_UNIQ_MAPQ10_200bp_SummedInput_bin25bp.bw')
+
+####
+
+sumBAMinputs(
+    bam.controls = "ce11_FRM_HiSeq_input.bam", 
+    bw.mappability = '../../../_mappability_files_/ce11_gem-mappability_36bp.bw', 
+    genome = 'ce11',
+    out_name = "ce11_FRM_HiSeq_UNIQ_MAPQ10_200bp",
+    uniq = TRUE, 
+    insert = 200L, 
+    mapq_cutoff = 10L, 
+    quickMap = TRUE,
+    bin = 25L
+)
+file.rename('ce11_FRM_HiSeq_UNIQ_MAPQ10_200bp_SummedInput_linear_25bp.bw', 'ce11_FRM_HiSeq_UNIQ_MAPQ10_200bp_SummedInput_bin25bp.bw')
+
+
+sumBAMinputs(
+    bam.controls = "ce11_EGS_HiSeq_input.bam", 
+    bw.mappability = '../../../_mappability_files_/ce11_gem-mappability_36bp.bw', 
+    genome = 'ce11',
+    out_name = "ce11_EGS_HiSeq_UNIQ_MAPQ10_200bp",
+    uniq = TRUE, 
+    mapq_cutoff = 10L, 
+    insert = 200L, 
+    quickMap = TRUE,
+    bin = 25L
+)
+file.rename('ce11_EGS_HiSeq_UNIQ_MAPQ10_200bp_SummedInput_linear_25bp.bw', 'ce11_EGS_HiSeq_UNIQ_MAPQ10_200bp_SummedInput_bin25bp.bw')
+
