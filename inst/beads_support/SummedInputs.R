0766640dd842ba0fb19789a7045436503bfb9687
przemol <pstempor@gmail.com>
2018-01-14 17:08:20 +0000

    noMAPPABILITY BEADS for NQNU

--- inst/beads_support/SummedInputs.R
+++ inst/beads_support/SummedInputs.R
@@ -23,12 +23,12 @@ sumBAMinputs(
 )
 
 
-
+# nonMAP nonUNIQ noMAPPABILITY
 sumBAMinputs(
     bam.controls = "ce11_FRM_HiSeq_input.bam", 
-    bw.mappability = '../../../_mappability_files_/ce11_gem-mappability_36bp.bw', 
+    bw.mappability = NULL, 
     genome = 'ce11',
-    out_name = "ce11_FRM_HiSeq_nonUNIQ_nonMAPQ_200bp",
+    out_name = "ce11_FRM_HiSeq_nonUNIQ_nonMAPQ_noMAPPABILITY_200bp",
     uniq = FALSE, 
     insert = 200L, 
     mapq_cutoff = 0, 
@@ -38,9 +38,9 @@ sumBAMinputs(
 
 sumBAMinputs(
     bam.controls = "ce11_EGS_HiSeq_input.bam", 
-    bw.mappability = '../../../_mappability_files_/ce11_gem-mappability_36bp.bw', 
+    bw.mappability = NULL, 
     genome = 'ce11',
-    out_name = "ce11_EGS_HiSeq_nonUNIQ_nonMAPQ_200bp",
+    out_name = "ce11_EGS_HiSeq_nonUNIQ_nonMAPQ_noMAPPABILITY_200bp",
     uniq = FALSE, 
     mapq_cutoff = 0, 
     insert = 200L, 
