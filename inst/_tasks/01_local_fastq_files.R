c6c3231f9464552d62657199464848bfdc11fae6
JA_Lab Pipeline <jarun@cb-head2.gurdon.private.cam.ac.uk>
2018-08-02 14:32:42 +0100

    tasks added

---
+++ inst/_tasks/01_local_fastq_files.R
@@ -0,0 +1,7 @@
+# Submissions -> https://docs.google.com/spreadsheets/d/1-gfqOfVLQGciLfsewWy709VlYZF5VK3mgC6sh-UsBuo/edit?usp=sharing
+# or https://docs.google.com/spreadsheets/d/1-gfqOfVLQGciLfsewWy709VlYZF5VK3mgC6sh-UsBuo/edit#gid=1856818976
+# The field with paths - OryginalFileName
+
+jadb_dc('https://docs.google.com/spreadsheets/d/1jz6pnfbqGi3dENoDwx8f6RJirlHlz2LlOQIeOtnATPI/edit?usp=sharing', basespace = FALSE)
+
+
